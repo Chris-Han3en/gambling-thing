49e138407d85ace28dc239128c4bc4d5659eac25
Chris-Han3en <75200147+Chris-Han3en@users.noreply.github.com>
2021-05-14 21:38:19 +0100

    Update gamble thing.py

--- gamble thing.py
+++ gamble thing.py
@@ -1,37 +1,35 @@
-#this is my shitty gambiling thing ( i think i spelt that wrong )
 import random, time, os
 
 def input_verify(message):
     while True:
-        user_input = input(message).lower() #does shit
+        user_input = input(message).lower()
         if user_input in ("yes", "no"):
             return user_input
-        print("Invalid Input")
+        print("Invalid Input\nPlease use one of the formats 'yes' or 'no'.")
 
 def win(number_of_matches, fruit):
     global balance
-    print ("Well done! You got", number_of_matches, "of the same fruit!")#more shit lmao
+    print ("Well done! You got", number_of_matches, "of the same fruit!")
     time.sleep(0.5)
     reward = fruits[fruit][1] * number_of_matches
-    print("You won £{}!".format(reward))
+    print("You won {} doge coin!".format(reward))
     balance += reward
 
 fruits = {"apple": [10000, 1], "banana": [7000, 1.5], "lemon": [6000, 1.75], "orange": [5000, 2], "kiwi": [4000, 4],
           "peach": [3000, 6], "avocado": [2000, 10], "grapes": [1000, 20], "mango": [500, 100], "melon": [1, 10000]}
 weighted_fruits = [fruit for fruit in fruits for i in range(fruits[fruit][0])]
 
-print("Welcome to my even even even even even even better version of ewans better version of my better version of his ball game")
 print("Each spin costs 1 doge coin")#doge coin best coin
 balance = 10
-if os.path.exists("One Armed Bandit.txt"):
-    with open("One Armed Bandit.txt", "r") as f:
+if os.path.exists("SavedData.txt"):
+    with open("SavedData.txt", "r") as f:
         contents = f.readlines()
-    continue_saved_game = input_verify("Do you want to continue your saved game?")
+    continue_saved_game = input_verify("Do you want to continue your saved game?\n")
     if continue_saved_game == "yes":
         balance = float(contents[1].strip())
 
 time.sleep(0.5)
-print("\nYour current balance is £{}.\n".format(balance))#idk why its now £ and not doge coin but i cba to change it
+print("\nYour current balance is {} doge coin.\n".format(balance))
 time.sleep(1)
 while True:
     balance -= 1
@@ -52,16 +50,15 @@ while True:
     else:
         print("Unlucky. None of your fruits matched each other.")
     time.sleep(0.5)
-    print("\nYour current balance is £{}.\n".format(balance))
+    print("\nYour current balance is {} doge coin.\n".format(balance))
     time.sleep(0.5)
-    replay = input_verify("Do you want to play again?")
+    replay = input_verify("Do you want to play again?\n")
     if replay == "no":
-        save = input_verify("Do you want to save your game?")#idek if the saving works lmao 
+        save = input_verify("Do you want to save your game?\n")
         if save == "yes":
-            name = input("What is you name?")
-            with open (name + ".txt", "w") as f:
-                f.write("{}'s Balance:\n{}".format(name, balance))
+            with open ("SavedData.txt", "w") as f:
+                f.write("players Balance:\n{}".format(balance))
+                print("game saved.")
         break
 #made by chris hansen#3106
 #<3
-#there is a undefined error but im a lazy programmer and cba to fix it
