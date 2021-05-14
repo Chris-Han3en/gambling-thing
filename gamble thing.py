46f618c55bad5d724b42e40f4771af4101d3fde3
Chris-Han3en <75200147+Chris-Han3en@users.noreply.github.com>
2021-05-14 21:56:05 +0100

    Update gamble thing.py

--- gamble thing.py
+++ gamble thing.py
@@ -5,14 +5,14 @@ def input_verify(message):
         user_input = input(message).lower()
         if user_input in ("yes", "no"):
             return user_input
-        print("Invalid Input\nPlease use one of the formats 'yes' or 'no'.")
+        print("Invalid Input\nPlease use one of the formats 'yes' or 'no'.\n-------------------------------------")
 
 def win(number_of_matches, fruit):
     global balance
     print ("Well done! You got", number_of_matches, "of the same fruit!")
     time.sleep(0.5)
     reward = fruits[fruit][1] * number_of_matches
-    print("You won {} doge coin!".format(reward))
+    print("You won {} doge coin!\n-------------------------------------".format(reward))
     balance += reward
 
 fruits = {"apple": [10000, 1], "banana": [7000, 1.5], "lemon": [6000, 1.75], "orange": [5000, 2], "kiwi": [4000, 4],
@@ -35,7 +35,7 @@ while True:
     balance -= 1
     reward = 0
     a, b, c = [random.choice(weighted_fruits) for i in range(3)]
-    print("Spinning...")
+    print("\n-------------------------------------\nSpinning...")
     for fruit in (a, b, c):
         time.sleep(0.5)
         print(fruit)
@@ -48,17 +48,18 @@ while True:
         else:
             win(2, a)
     else:
-        print("Unlucky. None of your fruits matched each other.")
+        print("Unlucky. None of your fruits matched each other.\n-------------------------------------")
     time.sleep(0.5)
     print("\nYour current balance is {} doge coin.\n".format(balance))
     time.sleep(0.5)
-    replay = input_verify("Do you want to play again?\n")
+    replay = input_verify("Do you want to play again?\n-------------------------------------\n")
     if replay == "no":
         save = input_verify("Do you want to save your game?\n")
         if save == "yes":
             with open ("SavedData.txt", "w") as f:
                 f.write("players Balance:\n{}".format(balance))
                 print("game saved.")
+                input ("please press 'enter' to close the game.")
         break
 #made by chris hansen#3106
 #<3
